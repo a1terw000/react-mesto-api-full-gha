f488ddd8b9afb214b30439342378f32dd1a8ffe9
Danil Baidak <lazya142425@gmail.com>
2023-12-10 00:15:06 +0300

    15pr

--- backend/routes/signin.js
+++ backend/routes/signin.js
@@ -5,7 +5,7 @@ const { login } = require('../controllers/users');
 router.post('/', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(4),
+    password: Joi.string().required().min(3),
   }),
 }), login);
 
--- backend/routes/signup.js
+++ backend/routes/signup.js
@@ -8,7 +8,7 @@ router.post('/', celebrate({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(4),
+    password: Joi.string().required().min(3),
     avatar: Joi
       .string()
       .pattern(regexUrl),
